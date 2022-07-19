ea0c06df1063836a0096b343cb4cfbd5ed7bb3c0
Jesse Ojih <jcinvent05@gmail.com>
2022-07-19 10:11:00 +0100

    Generate Seqwit BTC address

--- addresses/generateBTCAddressFromPublicKey.js
+++ addresses/generateBTCAddressFromPublicKey.js
@@ -6,5 +6,11 @@ module.exports = (publicKey) => {
     network: Bitcoin.networks.testnet,
   });
 
+  // p2ms - Pay 2 Multisig
+  const segwit = Bitcoin.payments.p2wpkh({
+    pubkey: Buffer.from(publicKey, "hex"),
+    network: Bitcoin.networks.testnet,
+  }).address;
+
   return result.address;
 };
